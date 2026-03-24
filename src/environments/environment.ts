9d47d5bf70e3289b97a3a3f0800c5eaf037d4d39
AlexArce2000 <alexisroarce2000@gmail.com>
2026-03-24 16:52:28 -0300

    fix: enviroments local reparado

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "REPLACE_ME",
+    authDomain: "REPLACE_ME",
+    projectId: "REPLACE_ME",
+    storageBucket: "REPLACE_ME",
+    messagingSenderId: "REPLACE_ME",
+    appId: "REPLACE_ME",
+    measurementId: "REPLACE_ME"
+  }
+};
